c94b7dfda6243bf7394f0c64ad76dfb631e0d35d
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-04-29 17:17:36 +0100

    Initial commit

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,11 @@
     global $:false
 */
 
-if (typeof jQuery === 'undefined') {throw new Error('Include jQuery you fool!');}
-
-var hello = 'Hello World';
-
-jQuery(function() {
-    'use strict';
-    console.log(hello);
-});
+//
+//
+//var hello = 'Hello World';
+//
+//jQuery(function() {
+//    'use strict';
+//    console.log(hello);
+//});
