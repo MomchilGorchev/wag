84dd9f3b676c7d6c53fc02054b6958f6c3d254f2
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-07-27 17:58:07 +0100

    Trying to make it work with Youtube links

--- app/js/main2.js
+++ app/js/main2.js
@@ -10,14 +10,28 @@ var yCenter = HEIGHT /2;
 //console.log(canvas.width);
 //console.log(HEIGHT);
 //console.log(canvas.height);
+var audio;
+var inputURL = document.querySelector('#audio-src');
+var submitURL = document.querySelector('#save-audio-src');
+
+submitURL.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    var url = inputURL.value;
+
+    audio = new Audio();
+    audio.src = url;
+    audio.controls = true;
+    audio.autoplay = true;
+    document.body.appendChild(audio);
+
+    initMusicVisualization()
+
+});
 
 
-var audio = new Audio();
-audio.src = '../music/song1.mp3';
-audio.controls = true;
-audio.autoplay = false;
-document.body.appendChild(audio);
-
+function initMusicVisualization(){
 
 //
 //navigator.getUserMedia = ( navigator.getUserMedia    || navigator.webkitGetUserMedia ||
@@ -26,76 +40,77 @@ document.body.appendChild(audio);
 //if (navigator.getUserMedia) {
 //    navigator.getUserMedia({audio: true}, function(stream) {
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var analyser = audioCtx.createAnalyser();
-var gainNode = audioCtx.createGain();
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioCtx.createAnalyser();
+    var gainNode = audioCtx.createGain();
 
-source = audioCtx.createMediaElementSource(audio);
-source.connect(analyser);
-analyser.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-
-
-analyser.fftSize = 32;
-
-var bufferLength = analyser.frequencyBinCount;
-console.log(bufferLength);
-var dataArray = new Uint8Array(bufferLength);
-
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    source = audioCtx.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
 
 
-function draw() {
+    analyser.fftSize = 32;
 
-    requestAnimationFrame(draw);
-
-    analyser.getByteFrequencyData(dataArray);
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
 
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    var barWidth = (WIDTH / bufferLength) * 2.5;
-    var barHeight;
-    var x = 0;
 
-    for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i]/2;
+    function draw() {
 
-        var color = {
-            r: Math.floor(Math.random() * 255),
-            g:  Math.floor(Math.random() * 255),
-            b:  Math.floor(Math.random() * 255),
-            a: Math.random()
-        };
+        requestAnimationFrame(draw);
 
-        ctx.strokeStyle = 'rgba(255, 255, 255, '+ color.a +')';
-        //ctx.strokeStyle = 'rgba('+ 0 +', '+ 100 +', '+ color.b +', '+ color.a +')';
-        ctx.lineWidth = 4;
-        ctx.beginPath();
+        analyser.getByteFrequencyData(dataArray);
 
-        var radius = barHeight + barWidth / 2;
-        ctx.arc(xCenter, yCenter, radius, 0, Math.PI * 2, false);
-        ctx.closePath();
-        ctx.stroke();
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-        if(radius > 200){
-            ctx.lineWidth = 1;
-            ctx.moveTo(xCenter, yCenter);
-            ctx.lineTo(Math.random() * WIDTH, Math.random() * HEIGHT);
-            //ctx.stroke();
+        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        for(var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i]/2;
+
+            var color = {
+                r: Math.floor(Math.random() * 255),
+                g:  Math.floor(Math.random() * 255),
+                b:  Math.floor(Math.random() * 255),
+                a: Math.random()
+            };
+
+            ctx.strokeStyle = 'rgba(255, 255, 255, '+ color.a +')';
+            //ctx.strokeStyle = 'rgba('+ 0 +', '+ 100 +', '+ color.b +', '+ color.a +')';
+            ctx.lineWidth = 4;
+            ctx.beginPath();
+
+            var radius = barHeight + barWidth / 2;
+            ctx.arc(xCenter, yCenter, radius, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.stroke();
+
+            if(radius > 100){
+                ctx.lineWidth = 1;
+                ctx.moveTo(xCenter, yCenter);
+                ctx.lineTo(Math.random() * WIDTH, Math.random() * HEIGHT);
+                //ctx.stroke();
+            }
+
+
+            ////
+
+            //console.log();
+
+            //ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+            //ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
+
+            x += barWidth + 1;
         }
-
-
-        ////
-
-        //console.log();
-
-        //ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-        //ctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
-
-        x += barWidth + 1;
     }
-}
 
-draw();
+    draw();
+}
